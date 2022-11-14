52c9e5938ce5e5257be21040a00d13c11f5ec9a9
Oluwadara <oluwadarasimi5000@gmail.com>
2022-11-14 14:14:04 +0100

    removed folders and updated js code

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function openPanel() {
-  document.getElementById("panel").style.height = "270px";
+  document.getElementById("panel").style.height = "350px";
   document.getElementById("collapseButton").style.backgroundColor =
     "rgb(58, 51, 51)";
+  document.getElementById("collapsedDiv").style.display = "block";
 }
 
 function closePanel() {
   document.getElementById("panel").style.height = "0";
   document.getElementById("collapseButton").style.backgroundColor =
     "rgb(238, 245, 227)";
+   document.getElementById("collapsedDiv").style.display = "none"; 
 }
